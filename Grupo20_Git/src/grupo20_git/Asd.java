03c3a228cd6ff43c8df15b944a3b27e98c904a70
NehuenZ12 <aldonehuen123@gmail.com>
2025-08-22 23:48:45 -0300

    Update Asd.java

--- Grupo20_Git/src/grupo20_git/Asd.java
+++ Grupo20_Git/src/grupo20_git/Asd.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package grupo20_git;
+import java.util.Scanner;
+import java.util.Random;
 
-/**
- *
- * @author Nehuen
- */
-public class Asd {
+public class Castor {
     
+    private double longitudCola; 
+    private double velocidad;   
+
+
+    public Castor() {
+    }
+
+    // Constructor con parámetros
+    public Castor(double longitudCola, double velocidad) {
+        this.longitudCola = longitudCola;
+        this.velocidad = velocidad;
+    }
+
+    public void cargarDatos() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese longitud de la cola (cm): ");
+        this.longitudCola = sc.nextDouble();
+        System.out.print("Ingrese velocidad de nado (km/h): ");
+        this.velocidad = sc.nextDouble();
+    }
+
+    public void nadar() {
+        System.out.println("El castor nada a una velocidad de " + velocidad + " km/h.");
+    }
+
+    public void tocarGuitarra() {
+        Random random = new Random();
+        System.out.println("El castor comienza a tocar la guitarra...");
+        for (int i = 0; i < 3; i++) {
+            int cuerda = random.nextInt(6) + 1; 
+            System.out.println("Toca cuerda " + cuerda);
+        }
+    }
+
+    public double getLongitudCola() {
+        return longitudCola;
+    }
+
+    public void setLongitudCola(double longitudCola) {
+        this.longitudCola = longitudCola;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Castor [longitudCola=" + longitudCola + " cm, velocidad=" + velocidad + " km/h]";
+    }
 }
